9eb73a1e1db75a03223f531ba3bdcf060ebc153b
rekasj <56637522+rekasj@users.noreply.github.com>
2021-10-27 23:45:59 +0200

    Initial Commit

---
+++ PracaInzynierska/PracaInzynierskaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracaInzynierskaApp.swift
+//  PracaInzynierska
+//
+//  Created by Jakub Rękas on 27/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PracaInzynierskaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
